46e3b25db5dfec6f06856fb2b53e57944d6c00e9
prakash-2002 <lovelyprakash650@gmail.com>
2024-09-08 00:40:28 +0530

    color flipper created

---
+++ color_flipper/colorflipp.js
@@ -0,0 +1,23 @@
+const colortxt = document.getElementById("color")
+const warp = document.getElementById("warp")
+const btn = document.getElementById("btn")
+
+const hex=[0, 1, 2, 3, 4, 6, 7, 8, 9, 'A','B','C','D','E','F']
+
+btn.addEventListener('click', changebg)
+
+
+function changebg(){
+    let hexcolor = '#'
+    for (let i=1;i<=6;i++){
+        hexcolor += randHexVal()
+    }
+    console.log(hexcolor)
+    colortxt.innerHTML=hexcolor
+    warp.style.backgroundColor=hexcolor
+}
+
+function randHexVal(){
+    let randomindex= Math.floor(Math.random()*15)
+    return hex[randomindex]
+}
